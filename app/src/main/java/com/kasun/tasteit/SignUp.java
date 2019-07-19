2e8b23d9e4d1eba6120b6fbfdb9f7fdc3206743a
hewagekasunthilina <kasunthilina1000@gmail.com>
2019-07-19 23:59:45 +0530

    Sign In Sign Up create Successfully

---
+++ app/src/main/java/com/kasun/tasteit/SignUp.java
@@ -0,0 +1,79 @@
+package com.kasun.tasteit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.kasun.tasteit.Model.User;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignUp extends AppCompatActivity {
+
+    MaterialEditText edtPhone,edtName,edtPassword;
+    Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edtName = (MaterialEditText)findViewById(R.id.edtName);
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        //Init Firebase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_User = database.getReference("User");
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
+                mDialog.setMessage("Please Waiting...");
+                mDialog.show();
+
+                table_User.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        //Check if already user phone in the database
+                        if(dataSnapshot.child(edtPhone.getText().toString()).exists())
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(SignUp.this, "Phone Number already Registered!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        else
+                        {
+                            mDialog.dismiss();
+                            User user = new User(edtName.getText().toString(),edtPassword.getText().toString());
+                            table_User.child(edtPhone.getText().toString()).setValue(user);
+
+                            Toast.makeText(SignUp.this, "Sign Up Successfully!", Toast.LENGTH_SHORT).show();
+                            finish();
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
