071ecf2f1d3fb108e40e211c7e5cf00b37e10ac7
hewagekasunthilina <kasunthilina1000@gmail.com>
2019-07-26 23:06:21 +0530

    Menu item layout added

--- app/src/main/java/com/kasun/tasteit/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/com/kasun/tasteit/ViewHolder/MenuViewHolder.java
@@ -1,4 +1,22 @@
 package com.kasun.tasteit.ViewHolder;
 
-public class MenuViewHolder {
-}
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.support.v7.widget.RecyclerView;
+
+    public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView txtMenuName;
+        public ImageView imageView;
+
+        public MenuViewHolder(View itemView){
+            super(itemView);
+
+        }
+        @Override
+        public void onClick(View view){
+
+        }
+    }
