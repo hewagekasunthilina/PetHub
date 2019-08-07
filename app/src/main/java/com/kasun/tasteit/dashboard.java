8466d77076aa3aafc213a199caa05a51b93727e9
hewagekasunthilina <kasunthilina1000@gmail.com>
2019-08-08 02:02:22 +0530

    Dashboard updated

--- app/src/main/java/com/kasun/tasteit/dashboard.java
+++ app/src/main/java/com/kasun/tasteit/dashboard.java
@@ -2,13 +2,29 @@ package com.kasun.tasteit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class dashboard extends AppCompatActivity {
+
+    ImageButton imgButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        imgButton = (ImageButton)findViewById(R.id.arr);
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(dashboard.this, category_list.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
