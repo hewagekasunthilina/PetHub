f5f88a706b3d00b40844398377aff228fa661388
hewagekasunthilina <kasunthilina1000@gmail.com>
2019-08-08 00:58:32 +0530

    Category list files and category DB created successfully

--- app/src/main/java/com/kasun/tasteit/Interface/ItemClickListner.java
+++
@@ -1,8 +0,0 @@
-package com.kasun.tasteit.Interface;
-
-import android.view.View;
-
-public interface ItemClickListner {
-
-    void onClick(View view, int position, boolean isLongClick);
-}
--- app/src/main/java/com/kasun/tasteit/SignUp.java
+++ app/src/main/java/com/kasun/tasteit/SignUp.java
@@ -7,7 +7,6 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
