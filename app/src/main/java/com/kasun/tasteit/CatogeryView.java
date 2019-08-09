13f5dd2a4ae06d81f15ec3a383da2a9d13d14b10
AASMadhawa <madhawa.amarasinghe96@gmail.com>
2019-08-09 20:08:58 +0530

    Equipment category page added

---
+++ app/src/main/java/com/kasun/tasteit/CatogeryView.java
@@ -0,0 +1,14 @@
+package com.kasun.tasteit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatogeryView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catogery_view);
+    }
+}
